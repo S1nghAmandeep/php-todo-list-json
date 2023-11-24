9c2f582722fcd6043284c160f33a9f6a45d17d6d
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-24 15:34:22 +0100

    add-base html tags/ linked axios/vue/js file

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <title>To Do List</title>
+</head>
+
+<body>
+    <div id="app">
+
+    </div>
+
+    <script src="./app/app.js"></script>
+</body>
+
+</html>
