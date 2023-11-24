5fa642e64384d7d1ccb7e63d1fc0e762f34fe1cd
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-24 17:03:54 +0100

    sent data via axios

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ createApp({
         return {
             message: 'Hello Vue!',
             todos: [],
+            newTodos: '',
         }
     },
     methods: {
@@ -12,6 +13,24 @@ createApp({
             axios.get('server.php').then(res => {
                 console.log(res.data);
                 this.todos = res.data
+            })
+        },
+
+        addTodos() {
+            // console.log(this.newTodos);
+
+            const data = {
+                text: this.newTodos,
+                done: true
+            }
+            axios.post('store.php', data, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                }
+            }).then(res => {
+                console.log(res.data);
+                this.todos = res.data
+                this.newTodos = ''
             })
         }
     },
