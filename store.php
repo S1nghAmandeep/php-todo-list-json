9e2355a89c8d62224f2f2879aee7346a99867ad4
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-24 18:44:41 +0100

    solved-isssue with var_dump

--- store.php
+++ store.php
@@ -7,17 +7,17 @@ $todo = [
     "done" => true
 ];
 
-var_dump($new_todo);
+//var_dump($new_todo);
 
 $todo_array = file_get_contents('./todos.json');
 $todos = json_decode($todo_array, true);
 
 
 $todos[] = $todo;
-var_dump($todos);
+//var_dump('ciao');
 
 $todo_array = json_encode($todos);
 file_put_contents('./todos.json', $todo_array);
 
-header('Content-type: application/json');
+header('Content-Type: application/json');
 echo $todo_array;
