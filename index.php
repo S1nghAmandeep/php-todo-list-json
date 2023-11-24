e069430bb82b60bd63e39237a5511c194ae5cdd1
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-24 18:44:20 +0100

    add-bootstrap

--- index.php
+++ index.php
@@ -6,18 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
     <title>To Do List</title>
 </head>
 
 <body>
     <div id="app">
-        <h1>To do list</h1>
-        <input type="text" v-model="newTodos" @keyup.enter="addTodos()">
-        <ul>
-            <li v-for="(todo, i) in todos">
-                {{todo.text}}
-            </li>
-        </ul>
+        <div class="container">
+            <h1>To do list</h1>
+            <div class="add-task">
+                <input class="form-control" type="text" v-model="newTodos" @keyup.enter="addTodos()">
+                <button class="btn btn-outline-success" @click="newTodos">Aggiungi</button>
+            </div>
+            <div class="mt-5">
+                <ul class="list-group">
+                    <li class="list-group-item list-group-item-success d-flex justify-content-between align-items-center" v-for="(todo, i) in todos">
+                        {{todo.text}}
+                    </li>
+                </ul>
+            </div>
+        </div>
 
     </div>
 
